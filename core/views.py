159002789f2792f5b9ba65a15d515b4dd9d1d958
Vitor Angheben <vitor_angheben@hotmail.com>
2023-07-13 09:53:45 -0300

    Working in the HTML file

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    """
+    This class will serve as a template for the base.html file
+    """
+    template_name = "base.html"
